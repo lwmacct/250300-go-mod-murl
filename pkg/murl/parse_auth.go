dd47f67c9219eb27e2099388b3a9f15f706ee509
lwmacct <lwmacct@icloud.com>
2025-04-10 11:13:26 +0800

    -

--- pkg/murl/parse_auth.go
+++ pkg/murl/parse_auth.go
@@ -2,24 +2,24 @@ package murl
 
 import (
 	"fmt"
-	"net/url"
+	stdUrl "net/url"
 )
 
 // ParseAuth 解析 URL 中的用户名和密码, 去除并返回 URL, Username, Password
-func ParseAuth(aURL string) (URL string, Username string, Password string, err error) {
+func ParseAuth(aURL string) (url string, username string, password string, err error) {
 	if aURL == "" {
 		err = fmt.Errorf("URL is empty")
 		return
 	}
 
-	urlParse, err := url.Parse(aURL)
+	urlParse, err := stdUrl.Parse(aURL)
 	if err != nil {
 		err = fmt.Errorf("URL parse error: %v", err)
 		return
 	}
 
-	URL = fmt.Sprintf("%s://%s%s%s", urlParse.Scheme, urlParse.Host, urlParse.Path, urlParse.RawQuery)
-	Username = urlParse.User.Username()
-	Password, _ = urlParse.User.Password()
+	url = fmt.Sprintf("%s://%s%s%s", urlParse.Scheme, urlParse.Host, urlParse.Path, urlParse.RawQuery)
+	username = urlParse.User.Username()
+	password, _ = urlParse.User.Password()
 	return
 }
