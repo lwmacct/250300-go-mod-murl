9685c8d54ebfb4caebc5feb73a13deaa61d6d86b
lwmacct <lwmacct@icloud.com>
2025-04-10 11:22:54 +0800

    -

--- pkg/murl/parse_auth.go
+++ pkg/murl/parse_auth.go
@@ -2,24 +2,42 @@ package murl
 
 import (
 	"fmt"
-	stdUrl "net/url"
+	"net/url"
 )
 
+type Parse struct {
+	Scheme   string
+	Host     string
+	Port     string
+	Path     string
+	RawQuery string
+	Username string
+	Password string
+
+	NotAuthURL string
+}
+
 // ParseAuth 解析 URL 中的用户名和密码, 去除并返回 URL, Username, Password
-func ParseAuth(aURL string) (url string, username string, password string, err error) {
-	if aURL == "" {
-		err = fmt.Errorf("URL is empty")
-		return
+func NewParse(URL string) (*Parse, error) {
+	if URL == "" {
+		return nil, fmt.Errorf("URL is empty")
 	}
 
-	urlParse, err := stdUrl.Parse(aURL)
+	p, err := url.Parse(URL)
 	if err != nil {
-		err = fmt.Errorf("URL parse error: %v", err)
-		return
+		return nil, fmt.Errorf("URL parse error: %v", err)
 	}
 
-	url = fmt.Sprintf("%s://%s%s%s", urlParse.Scheme, urlParse.Host, urlParse.Path, urlParse.RawQuery)
-	username = urlParse.User.Username()
-	password, _ = urlParse.User.Password()
-	return
+	info := &Parse{
+		Scheme:   p.Scheme,
+		Host:     p.Host,
+		Port:     p.Port(),
+		Path:     p.Path,
+		RawQuery: p.RawQuery,
+	}
+	info.NotAuthURL = fmt.Sprintf("%s://%s%s%s", p.Scheme, p.Host, p.Path, p.RawQuery)
+	info.Username = p.User.Username()
+	info.Password, _ = p.User.Password()
+
+	return info, nil
 }
